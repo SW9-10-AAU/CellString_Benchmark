be9fde4810eb80c5ebf014362be1c63c7a9f6e13
Thomas Bjeldbak Madsen <75805327+thomasbjeldbak@users.noreply.github.com>
2026-04-08 09:15:53 +0200

    Remove unwanted folders
    This fork is meant to only encompass the benchmark framework for CellString.

--- queries/Query_2.sql
+++
@@ -1,5 +0,0 @@
-SELECT (ARRAY[1,2]::cellstring) && (ARRAY[2,3]::cellstring) AS Intersects;
-SELECT (ARRAY[1,2]::cellstring) & (ARRAY[2,3]::cellstring) AS Intersection;
-SELECT (ARRAY[1,2]::cellstring) | (ARRAY[2,3]::cellstring) AS Union;
-SELECT (ARRAY[1,3]::cellstring) - (ARRAY[3]::cellstring) AS Difference;
-SELECT (ARRAY[1,2,3]::cellstring) @>~ (ARRAY[2,3]::cellstring) AS Contains;
--- utils/get_cells_from_traj_or_stop.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    traj_ls.trajectory_id,
-    ST_NumPoints(traj_ls.geom) AS num_points,
-    cardinality(traj_cs.cellstring_z21) AS num_cells
-FROM prototype2.trajectory_ls AS traj_ls
-JOIN prototype2.trajectory_cs AS traj_cs
-    ON traj_ls.trajectory_id = traj_cs.trajectory_id
-WHERE traj_ls.trajectory_id = 103078;

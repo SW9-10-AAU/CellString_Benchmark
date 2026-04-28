796486c029a4f677872d75dda1290e0eb21e3034
Thomas Bjeldbak Madsen <75805327+thomasbjeldbak@users.noreply.github.com>
2026-04-28 09:53:31 +0200

    Change the passage query benchmark to take in the passage name, instead of ID

--- benchmarking/benchmarks/passage_query_benchmark.py
+++ benchmarking/benchmarks/passage_query_benchmark.py
@@ -11,9 +11,9 @@ from benchmarking.table_config import (
 )
 
 ST_SETUP_SQL = """
-SET VARIABLE crossing_id_a = ?;
-SET VARIABLE crossing_id_b = ?;
-SET VARIABLE crossing_id_c = ?;
+SET VARIABLE crossing_name_a = ?;
+SET VARIABLE crossing_name_b = ?;
+SET VARIABLE crossing_name_c = ?;
 """
 
 ST_SQL = """
@@ -24,9 +24,9 @@ FROM {trajectory_ls_table} AS traj,
      {crossing_ls_table} AS crossingA,
      {crossing_ls_table} AS crossingB,
      {crossing_ls_table} AS crossingC
-WHERE crossingA.crossing_id = getvariable('crossing_id_a')
-  AND crossingB.crossing_id = getvariable('crossing_id_b')
-  AND crossingC.crossing_id = getvariable('crossing_id_c')
+WHERE crossingA.name = getvariable('crossing_name_a')
+  AND crossingB.name = getvariable('crossing_name_b')
+  AND crossingC.name = getvariable('crossing_name_c')
   AND ST_Intersects(traj.geom, crossingA.geom)
   AND ST_Intersects(traj.geom, crossingB.geom)
   AND ST_Intersects(traj.geom, crossingC.geom);
@@ -36,9 +36,9 @@ WHERE crossingA.crossing_id = getvariable('crossing_id_a')
 )
 
 CST_SETUP_SQL = """
-SET VARIABLE crossing_id_a = ?;
-SET VARIABLE crossing_id_b = ?;
-SET VARIABLE crossing_id_c = ?;
+SET VARIABLE crossing_name_a = ?;
+SET VARIABLE crossing_name_b = ?;
+SET VARIABLE crossing_name_c = ?;
 """
 
 CST_SQL = """
@@ -47,13 +47,13 @@ SELECT
     t.mmsi
 FROM {trajectory_cs_table} t
 JOIN {crossing_cs_table} c ON t.cell_z21 = c.cell_z21
-WHERE c.crossing_id IN (
-    getvariable('crossing_id_a'),
-    getvariable('crossing_id_b'),
-    getvariable('crossing_id_c')
+WHERE c.name IN (
+    getvariable('crossing_name_a'),
+    getvariable('crossing_name_b'),
+    getvariable('crossing_name_c')
 )
 GROUP BY t.trajectory_id, t.mmsi
-HAVING COUNT(DISTINCT c.crossing_id) = 3;
+HAVING COUNT(DISTINCT c.name) = 3;
 """.format(
     trajectory_cs_table=TRAJECTORY_CS_TABLE,
     crossing_cs_table=CROSSING_CS_TABLE,
@@ -61,23 +61,26 @@ HAVING COUNT(DISTINCT c.crossing_id) = 3;
 
 
 def build_passage_query_benchmark(
-    crossing_id_a: int,
-    crossing_id_b: int,
-    crossing_id_c: int,
+    crossing_name_a: str,
+    crossing_name_b: str,
+    crossing_name_c: str,
 ) -> TimeBenchmark:
     return TimeBenchmark(
-        name=f"Passage query - crossings {crossing_id_a},{crossing_id_b},{crossing_id_c}",
+        name=(
+            "Passage query - crossings "
+            f"{crossing_name_a},{crossing_name_b},{crossing_name_c}"
+        ),
         st_sql=ST_SQL,
         cst_sql=CST_SQL,
         st_setup_sql=ST_SETUP_SQL,
         cst_setup_sql=CST_SETUP_SQL,
-        st_setup_params=(crossing_id_a, crossing_id_b, crossing_id_c),
-        cst_setup_params=(crossing_id_a, crossing_id_b, crossing_id_c),
+        st_setup_params=(crossing_name_a, crossing_name_b, crossing_name_c),
+        cst_setup_params=(crossing_name_a, crossing_name_b, crossing_name_c),
         params=tuple(),
         repeats=5,
     )
 
 
 PASSAGE_QUERY_BENCHMARKS: List[TimeBenchmark] = [
-    build_passage_query_benchmark(7, 11, 14),
+    build_passage_query_benchmark("Skagen", "Storebælt Syd", "Bornholms Gate"),
 ]
